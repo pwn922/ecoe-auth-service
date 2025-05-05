cce6d022effd4392f30da0622d08ef702ae09b8d
pwn922 <mauricio41533@gmail.com>
2025-05-04 19:39:03 -0800

    refactor(auth): update AuthModule to include RoleEntity and clean up unused imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,45 +3,52 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 import { AuthController } from './infrastructure/controllers/auth.controller';
 import { UserEntity } from './infrastructure/entities/user.entity.orm';
-import { AuthService } from './application/services/auth.service';
 
-import { RegisterUserUseCase } from './application/use-cases/register-user.use-case';
 import { LoginUserUseCase } from './application/use-cases/login-user.use-case';
 
 import { JwtServiceAdapter } from './infrastructure/services/jwt.service';
-import { BcryptServiceAdapter } from './infrastructure/services/bcrypt.service';
+//import { BcryptServiceAdapter } from './infrastructure/services/bcrypt.service';
 import { TypeOrmUserRepository } from './infrastructure/repositories/typeorm/user.repository';
+import { RoleEntity } from './infrastructure/entities/role.entity.orm';
+//import { RegisterUserUseCase } from './application/use-cases/register-user.use-case';
+import { TypeOrmRoleRepository } from './infrastructure/repositories/typeorm/role.repository';
+import { RegisterUserUseCase } from './application/use-cases/register-user.use-case';
+import { GoogleService } from './infrastructure/services/google.service';
 
-import { AuthUseCase } from './application/use-cases/auth.use-case';
-import { GoogleService } from './infrastructure/google/google.service';
+// import { AuthUseCase } from './application/use-cases/auth.use-case';
+//import { GoogleService } from './infrastructure/services/google.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forFeature([UserEntity]),
+    TypeOrmModule.forFeature([UserEntity, RoleEntity]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule,
   ],
   controllers: [AuthController],
   providers: [
-    AuthService,
     LoginUserUseCase,
     RegisterUserUseCase,
-    AuthUseCase,
+    // AuthUseCase,
     GoogleService,
     {
       provide: 'IUserRepositoryOutputPort',
       useClass: TypeOrmUserRepository,
     },
-
+    {
+      provide: 'IRoleRepositoryOutputPort',
+      useClass: TypeOrmRoleRepository,
+    },
+    /*
     {
       provide: 'IPasswordHasherOutputPort',
       useClass: BcryptServiceAdapter,
     },
+    */
     {
       provide: 'ITokenServiceOutputPort',
       useClass: JwtServiceAdapter,
