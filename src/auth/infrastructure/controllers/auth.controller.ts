412058ee2a86180b5193e55320a4c23c85e4ce3e
pwn922 <mauricio41533@gmail.com>
2025-05-04 22:48:55 -0800

    refact(auth): remove unnecessary blank line in AuthController constructor

--- src/auth/infrastructure/controllers/auth.controller.ts
+++ src/auth/infrastructure/controllers/auth.controller.ts
@@ -14,7 +14,6 @@ export class AuthController {
     constructor(
         private readonly loginUserUseCase: LoginUserUseCase,
         private readonly registerUserUseCase: RegisterUserUseCase,
-        
         private readonly googleService: GoogleService,
     ) {}
 
