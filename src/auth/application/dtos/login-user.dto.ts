d02056090a46a06a00fd1ca0ab1d69c63ab69a55
pwn922 <mauricio41533@gmail.com>
2025-05-04 19:28:16 -0800

    feat(dtos): add DTOs for user login, registration, and token payload

---
+++ src/auth/application/dtos/login-user.dto.ts
@@ -0,0 +1,6 @@
+
+
+export class LoginUserDto {
+    email: string;
+    userType: string;
+}
---
+++ src/auth/application/dtos/token-payload.dto.ts
@@ -0,0 +1,7 @@
+
+
+export interface TokenPayloadDto {
+    sub: string;
+    email: string;
+    role: string;
+}
