4af63447c56c04334bbab0a1de738de660866609
pwn922 <mauricio41533@gmail.com>
2025-05-04 19:28:45 -0800

    refactor(errors): remove InvalidCredentialsException class

--- src/auth/application/exceptions/invalid-credentials.exception.ts
+++
@@ -1,8 +0,0 @@
-
-
-export class InvalidCredentialsException extends Error {
-  constructor(message: string = 'Invalid credentials') {
-    super(message);
-    this.name = 'InvalidCredentialsException';
-  }
-}
