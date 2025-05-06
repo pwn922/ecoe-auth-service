e1803a252ad30a5a26dc2ec708e428be0179e18b
pwn922 <mauricio41533@gmail.com>
2025-05-05 18:55:15 -0800

    fix(login): update execute method to return TokenDto instead of string

--- src/auth/application/ports/in/login.in.port.ts
+++ src/auth/application/ports/in/login.in.port.ts
@@ -1,5 +1,6 @@
 import { LoginUserDto } from "../../dtos/login-user.dto";
+import { TokenDto } from "../../dtos/token.dto";
 
 export interface LoginUserInputPort {
-  execute(dto: LoginUserDto): Promise<string>;
+  execute(dto: LoginUserDto): Promise<TokenDto>;
 }
