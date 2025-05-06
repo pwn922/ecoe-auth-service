38c554e6bbed40ff583c198ac70ac5d19c08e506
pwn922 <mauricio41533@gmail.com>
2025-05-05 20:58:21 -0800

    feat(token-service): add verifyToken method to ITokenServiceOutputPort

--- src/auth/application/ports/out/token-service.output-port.ts
+++ src/auth/application/ports/out/token-service.output-port.ts
@@ -1,6 +1,8 @@
 import { TokenPayloadDto } from '../../dtos/token-payload.dto';
+import { TokenDto } from '../../dtos/token.dto';
 
 export interface ITokenServiceOutputPort {
     generateAccessToken(payload: TokenPayloadDto): Promise<string>;
     generateRefreshToken(payload: TokenPayloadDto): Promise<string>;
+    verifyToken(token: string): Promise<TokenDto>;
 }
