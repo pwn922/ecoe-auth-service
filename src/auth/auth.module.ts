78eb087efef38e59a6d656f0019f412ae1d07f2a
pwn922 <mauricio41533@gmail.com>
2025-05-08 21:02:44 -0800

    feat(auth): reorganize imports and update AuthModule configuration with new use cases and services

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,61 +1,75 @@
-// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
-
-import { AuthController } from './infrastructure/controllers/auth.controller';
-import { UserEntity } from './infrastructure/entities/user.entity.orm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { LoginUserUseCase } from './application/use-cases/login-user.use-case';
-
-import { JwtServiceAdapter } from './infrastructure/services/jwt.service';
-//import { BcryptServiceAdapter } from './infrastructure/services/bcrypt.service';
-import { TypeOrmUserRepository } from './infrastructure/repositories/typeorm/user.repository';
-import { RoleEntity } from './infrastructure/entities/role.entity.orm';
-//import { RegisterUserUseCase } from './application/use-cases/register-user.use-case';
-import { TypeOrmRoleRepository } from './infrastructure/repositories/typeorm/role.repository';
 import { RegisterUserUseCase } from './application/use-cases/register-user.use-case';
-import { GoogleService } from './infrastructure/services/google.service';
-import { VerifyTokenUseCase } from './application/use-cases/verify-token.use-case';
+import { GetUserUseCase } from './application/use-cases/get-user.use-case';
 
-// import { AuthUseCase } from './application/use-cases/auth.use-case';
-//import { GoogleService } from './infrastructure/services/google.service';
+import { AuthController } from './infrastructure/controllers/auth.controller';
+import { JwtStrategy } from './infrastructure/jwt/strategies/jwt.strategy';
+import { RoleEntity } from './infrastructure/entities/role.entity.orm';
+import { UserEntity } from './infrastructure/entities/user.entity.orm';
+import { GoogleServiceAdapter } from './infrastructure/services/google.service';
+import { JwtServiceAdapter } from './infrastructure/services/jwt.service';
+import { BcryptServiceAdapter } from './infrastructure/services/bcrypt.service';
+import { TypeOrmRoleRepository } from './infrastructure/repositories/typeorm/role.repository';
+import { TypeOrmUserRepository } from './infrastructure/repositories/typeorm/user.repository';
+import { VerifyOAuthUserUseCase } from './application/use-cases/verify-oauth-user.usecase';
+import { UserController } from './infrastructure/controllers/user.controller';
+import { TypeOrmLocalCredentialRepository } from './infrastructure/repositories/typeorm/local-credential.repository';
+import { LoginLocalUserUseCase } from './application/use-cases/login-local-user.usecase';
+import { LocalCredentialOrmEntity } from './infrastructure/entities/local-credential.entity.orm';
+import { RabbitMQServiceAdapter } from './infrastructure/services/rabbitmq.service';
+import { EmitStudentRegisterUseCase } from './application/use-cases/emit-student-register.use-case';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forFeature([UserEntity, RoleEntity]),
+    TypeOrmModule.forFeature([UserEntity, RoleEntity, LocalCredentialOrmEntity]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule,
   ],
-  controllers: [AuthController],
+  controllers: [AuthController, UserController],
   providers: [
+    JwtStrategy,
     LoginUserUseCase,
     RegisterUserUseCase,
-    VerifyTokenUseCase,
-    // AuthUseCase,
-    GoogleService,
+    GetUserUseCase,
+    VerifyOAuthUserUseCase,
+    LoginLocalUserUseCase,
+    EmitStudentRegisterUseCase,
     {
-      provide: 'IUserRepositoryOutputPort',
-      useClass: TypeOrmUserRepository,
+        provide: 'IOAuthAuthProviderOutPort',
+        useClass: GoogleServiceAdapter,
     },
     {
-      provide: 'IRoleRepositoryOutputPort',
-      useClass: TypeOrmRoleRepository,
+        provide: 'IUserRepositoryOutputPort',
+        useClass: TypeOrmUserRepository,
     },
-    /*
     {
-      provide: 'IPasswordHasherOutputPort',
-      useClass: BcryptServiceAdapter,
+        provide: 'IRoleRepositoryOutputPort',
+        useClass: TypeOrmRoleRepository,
     },
-    */
     {
-      provide: 'ITokenServiceOutputPort',
-      useClass: JwtServiceAdapter,
+        provide: 'ILocalCredentialRepositoryOutputPort',
+        useClass: TypeOrmLocalCredentialRepository,
+    },  
+    {
+        provide: 'IUserEventsOutPort',
+        useClass: RabbitMQServiceAdapter,
     },
-
+    {
+        provide: 'IPasswordHasherOutputPort',
+        useClass: BcryptServiceAdapter,
+    },
+    {
+        provide: 'ITokenManagerOutputPort',
+        useClass: JwtServiceAdapter,
+    },
   ],
 })
 export class AuthModule {}
