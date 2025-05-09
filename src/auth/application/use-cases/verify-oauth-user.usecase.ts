978079120273dee4f8de1de6a7ab3e83a823b4c2
pwn922 <mauricio41533@gmail.com>
2025-05-08 20:58:29 -0800

    feat(auth): add VerifyOAuthUserUseCase for OAuth token verification and remove VerifyTokenUseCase

---
+++ src/auth/application/use-cases/verify-oauth-user.usecase.ts
@@ -0,0 +1,24 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { VerifyOAuthUserInputPort } from '../ports/in/verify-oauth-user.in.port';
+import { IOAuthAuthProviderOutPort } from '../ports/out/oauth-provider.out.port';
+import { UnauthorizedAccessError } from '../errors/unauthorized-access.error';
+import { OAuthUser } from '../type/oauth-user.type';
+
+
+@Injectable()
+export class VerifyOAuthUserUseCase implements VerifyOAuthUserInputPort {
+    constructor(
+        @Inject('IOAuthAuthProviderOutPort')
+        private readonly oauthService: IOAuthAuthProviderOutPort,
+    ) {}
+
+    async execute(code: string): Promise<OAuthUser> {
+        const payload = await this.oauthService.verifyToken(code);
+
+        if (!payload) {
+            throw new UnauthorizedAccessError('Invalid OAuth token');
+        }
+
+        return payload;
+    }
+}
