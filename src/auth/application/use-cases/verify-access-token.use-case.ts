5f9d0e3aa1d4a100da6fd214e7f26a9b04cc91e5
pwn922 <mauricio41533@gmail.com>
2025-05-08 20:59:08 -0800

    feat(auth): implement VerifyAccessTokenUseCase for access token verification

---
+++ src/auth/application/use-cases/verify-access-token.use-case.ts
@@ -0,0 +1,17 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { TokenPayloadDto } from "../dtos/token-payload.dto";
+import { ITokenManagerPort } from "../ports/out/token-manager.out.port";
+
+
+@Injectable()
+export class VerifyAccessTokenUseCase {
+    constructor(
+        @Inject('ITokenManagerOutputPort')
+        private readonly tokenService: ITokenManagerPort,
+    ) {}
+
+    async execute(token: string): Promise<TokenPayloadDto | null> {
+        const decodedToken = await this.tokenService.verifyAccessToken(token);
+        return decodedToken ?? null;
+    }
+}
