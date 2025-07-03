e66eda00f295f2b29435497d45c0f50cd7ef04d4
pwn922 <mauricio41533@gmail.com>
2025-07-03 18:18:11 -0400

    feat(auth): add findAll method to IUserRepositoryOutputPort for retrieving all users

--- src/auth/domain/ports/out/user.repository.out.port.ts
+++ src/auth/domain/ports/out/user.repository.out.port.ts
@@ -5,6 +5,7 @@ export interface IUserRepositoryOutputPort {
     save(user: User): Promise<User>;
     findById(id: string): Promise<User | null>;
     findByEmail(email: string): Promise<User | null>;
+    findAll(): Promise<User[]>;
     update(user: User): Promise<void>;
     delete(id: string): Promise<void>;
 }
