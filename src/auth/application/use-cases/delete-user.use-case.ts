5652b900b0b20af6ca0e2a046125c3c6f6ebe9da
pwn922 <mauricio41533@gmail.com>
2025-07-04 02:46:41 -0400

    feat(auth): delete student data when deleting a user

--- src/auth/application/use-cases/delete-user.use-case.ts
+++ src/auth/application/use-cases/delete-user.use-case.ts
@@ -1,19 +1,29 @@
 import { Inject, Injectable } from "@nestjs/common";
-import { UserNotFoundError } from "src/auth/domain/errors/user-not-found.error";
 import { IUserRepositoryOutputPort } from "src/auth/domain/ports/out/user.repository.out.port";
+import { UserNotFoundError } from "src/auth/domain/errors/user-not-found.error";
+import { ClientProxy } from "@nestjs/microservices";
+import { firstValueFrom } from "rxjs";
 
 @Injectable()
 export class DeleteUserUseCase {
     constructor(
         @Inject('IUserRepositoryOutputPort')
-        private readonly userRepository: IUserRepositoryOutputPort
-    ) {}
+        private readonly userRepository: IUserRepositoryOutputPort,
+
+        @Inject('STUDENT_SERVICE')
+        private readonly studentClient: ClientProxy
+    ) { }
 
     async execute(userId: string): Promise<void> {
         const user = await this.userRepository.findById(userId);
-
         if (!user) {
             throw new UserNotFoundError('User not found');
+        }
+
+        if (user.getRole().toPrimitives().name === 'estudiante') {
+            await firstValueFrom(
+                this.studentClient.send('student.delete_by_user_id', { userId })
+            );
         }
 
         await this.userRepository.delete(user.getId());
